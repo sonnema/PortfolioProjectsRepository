2d7a795848629e574d5b330ef6807030faddeb61
sonnema <nemasonia@gmail.com>
2018-06-16 04:25:31 +0530

    update html and js

---
+++ PortfolioProjects/WebContent/WEB-INF/javascript/portfolio.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  
+  $('.navbar-nav a').click(function(){
+    $('.navbar-collapse').collapse('hide');
+  });
+  
+ $( "#ContactNav" ).click(function() {
+    $(function () {
+        $("#name").focus();
+    });
+  }); 
+  
+  
+//End of (document).ready function  
+});
+  
+ 
