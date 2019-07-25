a53f878626fc9c42274afc5d68e3899e375aceb0
sonnema <nemasonia@gmail.com>
2019-07-25 11:24:28 +0100

    Updated portfolio.html as per new standards

--- PortfolioProjects/WebContent/WEB-INF/javascript/portfolio.js
+++ PortfolioProjects/WebContent/WEB-INF/javascript/portfolio.js
@@ -1,16 +1,20 @@
+var subject = 'Client Enquiry Mail';
+var formData;
 $(document).ready(function(){
   
-  $('.navbar-nav a').click(function(){
-    $('.navbar-collapse').collapse('hide');
-  });
-  
- $( "#ContactNav" ).click(function() {
-    $(function () {
-        $("#name").focus();
-    });
-  }); 
-  
-  
+	$('.navbar-nav a').click(function(){
+		$('.navbar-collapse').collapse('hide');
+	});
+
+	$('.detailsClose').click(function(){
+		$(this).parent().collapse('hide');
+	});
+	
+	$( "#ContactNav" ).click(function() {
+		$(function () {
+			$("#name").focus();
+		});
+	});
 //End of (document).ready function  
 });
   
